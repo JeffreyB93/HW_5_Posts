1b8eb14bdda69ed4c2bf68922df128cea030d59e
BRO <baninro93@gmail.com>
2023-04-23 19:31:27 +0300

    second commit

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -94,4 +94,62 @@ class WallServiceTest {
 
         assertTrue(service.update(postUpdate))
     }
+
+    @Test
+    fun updateFalse() {
+        val service = WallService
+        val comment = Comment(1, canPost = true, groupsCanPost = false, canClose = true, canOpen = true)
+        val repost = Repost(12, userReposted = true)
+        val likes = Likes(12, userLikes = true, canLike = false, canPublish = true)
+
+        val photo = Photo()
+        val posted = Posted()
+        val video = Video()
+        val audio = Audio()
+        val doc = Doc()
+
+        val photoAttachment: Attachment = PhotoAttachment(photo)
+        val postedAttachment: Attachment = PostedAttachment(posted)
+        val videoAttachment: Attachment = VideoAttachment(video)
+        val audioAttachment: Attachment = AudioAttachment(audio)
+        val docAttachment: Attachment = DocAttachment(doc)
+
+        val attachments1: ArrayList<Attachment> = ArrayList()
+        attachments1.addAll(listOf(photoAttachment, postedAttachment))
+        val attachments2: ArrayList<Attachment> = ArrayList()
+        attachments2.addAll(listOf(postedAttachment, videoAttachment))
+        val attachments3: ArrayList<Attachment> = ArrayList()
+        attachments3.addAll(listOf(audioAttachment, docAttachment))
+
+
+        val postOne = Post(1, 2, 3, 4, null, "Первый пост",
+            2, 1,  true, comment, "qwe", likes, repost, 12,
+            "qwe", "asd","geo", attachments1,15, "tyu", canPin = true,
+            canDelete = true, canEdit = false, isPinned = false, markedAsAds = false, isFavorite = false,
+            12)
+
+        val postTwo = Post(1, 2, 3, 4, null, "Второй пост",
+            2, 1,  true, comment, "qwe", likes, repost, 12,
+            "qwe", "asd","geo", attachments2,15, "tyu", canPin = true,
+            canDelete = true, canEdit = false, isPinned = false, markedAsAds = false, isFavorite = false,
+            12)
+
+        val postThree = Post(1, 2, 3, 4, null, "Третий пост",
+            2, 1,  true, comment, "qwe", likes, repost, 12,
+            "qwe", "asd","geo", attachments3,15, "tyu", canPin = true,
+            canDelete = true, canEdit = false, isPinned = false, markedAsAds = false, isFavorite = false,
+            12)
+
+        val postUpdate = Post(5, 2, 3, 4, null, "Update пост",
+            2, 1,  true, comment, "qwe", likes, repost, 12,
+            "qwe", "asd","geo", attachments3,15, "tyu", canPin = true,
+            canDelete = true, canEdit = false, isPinned = false, markedAsAds = false, isFavorite = false,
+            12)
+
+        service.add(postOne)
+        service.add(postTwo)
+        service.add(postThree)
+
+        assertFalse(service.update(postUpdate))
+    }
 }
